742c812780766af252b0b271c4aafcb8a8e31317
Mathieu Lecarme <mlecarme@bearstech.com>
2015-10-07 21:59:13 +0200

    Count stuff.

--- tests/test_lepsius.py
+++ tests/test_lepsius.py
@@ -1,6 +1,8 @@
+from datetime import datetime
+
 import sys
 sys.path.append(".")
-from lepsius import count
+from lepsius import count, bucketize, group_by
 
 
 def test_count():
@@ -11,4 +13,21 @@ def test_count():
     assert c['fruit'] == 2
     assert c['vegetable'] == 1
 
+
+def test_bucketize():
+    a = [dict(timestamp=datetime(2015, 10, 7, hour=21, minute=33, second=27),
+              status='404'),
+         dict(timestamp=datetime(2015, 10, 7, hour=21, minute=33, second=42),
+              status='404'),
+         dict(timestamp=datetime(2015, 10, 7, hour=21, minute=33, second=27),
+              status='200'),
+         dict(timestamp=datetime(2015, 10, 7, hour=21, minute=42, second=27),
+              status='200'),
+         ]
+    for b in bucketize(a):
+        g = group_by(b, 'status')
+        for status, line in g.items():
+            print(status, len(line))
+
 test_count()
+test_bucketize()
